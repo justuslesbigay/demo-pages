8d073dfef63821c22830f75c06f387af991f8e66
csperando <csperando@gmail.com>
2025-05-12 09:43:26 -0600

    explore

---
+++ explore/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <title>Just Us!</title>
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+        <style>
+            * {
+                position: relative;
+                padding: 0;
+                margin: 0;
+                box-sizing: border-box;
+            }
+        </style>
+    </head>
+    <body>
+        <main class="w-[100vw] h-[100vh]">
+            <div class="w-full h-full flex flex-col items-center justify-center">
+                <p>Explore!</p>
+            </div>
+        </main>
+    </body>
+</html>
+</template>
