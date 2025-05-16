4ce180e089085bf46ae7d347c3be6860348f5e01
spiffytech <spiffy.git@bootspoon.com>
2025-05-16 17:13:16 -0400

    Matches: include room for icons on the match cards

--- matches/src/components/MatchCard.vue
+++ matches/src/components/MatchCard.vue
@@ -7,20 +7,24 @@ interface Match {
   age: number
 }
 
-const props = defineProps<{
+defineProps<{
   match: Match
 }>()
 </script>
 
 <template>
   <div class="relative">
-    <div class="absolute inset-0 flex items-end p-1">
+    <a href="" class="absolute inset-0 flex items-end p-1">
       <span
         class="backdrop-blur-md bg-black/50 text-white pl-2 pr-4 py-1 rounded-md whitespace-nowrap"
       >
         {{ match.name }}, {{ match.age }}
       </span>
-    </div>
+    </a>
+
+    <!-- icons -->
+    <span href="" class="absolute inset-0 text-white"></span>
+
     <img :src="match.image" class="w-full h-48 flex-auto object-cover rounded-md" />
   </div>
 </template>
