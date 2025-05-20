a6d4a829bb2295b63eaa9e0c4dfe6ac1d29fa97a
csperando <csperando@gmail.com>
2025-05-20 14:43:53 -0600

    add conditional to web history

--- matches/src/router/index.ts
+++ matches/src/router/index.ts
@@ -3,7 +3,7 @@ import MatchesView from '../views/MatchesView.vue'
 import MessagesView from '../views/MessagesView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL || '/demo-pages/matches/dist/'),
   routes: [
     {
       path: '/',
