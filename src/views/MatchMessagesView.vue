9a933ead9a926cb1bfdaacca7f729e1cdcc52d6c
spiffytech <spiffy.git@bootspoon.com>
2025-05-25 09:39:22 -0400

    Chat: Give send button a bigger hitbox

--- src/views/MatchMessagesView.vue
+++ src/views/MatchMessagesView.vue
@@ -97,7 +97,7 @@ const sendMessage = async (event: Event) => {
           class="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-brandpink-500 focus:border-transparent"
           placeholder="Type a message..."
         />
-        <button type="submit" class="text-2xl">
+        <button type="submit" class="text-2xl -ml-1 -mr-2 px-1">
           <PaperPlaneRight />
         </button>
       </form>
