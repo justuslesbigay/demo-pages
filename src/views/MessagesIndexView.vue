8cf5dcfbf7a7f8e76a978ed44e767406aaa15d28
spiffytech <spiffy.git@bootspoon.com>
2025-05-25 11:31:23 -0400

    Use colored icons in the messages index to show whether the message is inbound or outbound

--- src/views/MessagesIndexView.vue
+++ src/views/MessagesIndexView.vue
@@ -4,13 +4,18 @@ import { RouterLink } from 'vue-router'
 import MatchesNav from '@/components/matches/MatchesNav.vue'
 import MatchMiniPhoto from '@/components/matches/MatchMiniPhoto.vue'
 
+import ArrowSquareIn from '@/components/icons/ArrowSquareIn.vue'
+import ArrowSquareOut from '@/components/icons/ArrowSquareOut.vue'
+
 import { useMessagesStore } from '@/stores/messages'
 
 const store = useMessagesStore()
 </script>
 
 <template>
-  <main>
+  <main
+    class="bg-gradient-to-br from-[oklch(0.5614_0.228_328.1)] to-[oklch(0.7079_0.1109_201.76)] min-h-full"
+  >
     <MatchesNav />
 
     <div v-if="store.conversationsSorted.length === 0">No conversations</div>
@@ -33,7 +38,14 @@ const store = useMessagesStore()
           <header class="font-semibold text-xl">{{ conversation.match.name }}</header>
 
           <p v-if="conversation.messages.length === 0" class="italic">Start a conversation!</p>
-          <p v-else class="line-clamp-2">{{ conversation.messages.at(-1)?.message }}</p>
+          <p v-else class="line-clamp-2">
+            <ArrowSquareIn
+              v-if="conversation.messages.at(-1)?.direction === 'received'"
+              class="inline-block text-xl text-brandgreen-700"
+            />
+            <ArrowSquareOut v-else class="inline-block text-xl text-brandpink-500" />
+            {{ conversation.messages.at(-1)?.message }}
+          </p>
         </div>
       </li>
     </ul>
